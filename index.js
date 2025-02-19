16a069d52d54b8f8de602153ff281b130e7bb4cb
Pedro-456 <pedro.etec.botoso@gmail.com>
2025-02-19 17:51:55 +0000

    Aula 19/02/25 '0'

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const { ppid } = require('process');
+const app = express();
+
+const port = 3000;
+
+pp.listen(port, () => {
+    console.log('Servidor online na porta' + port)
+})
